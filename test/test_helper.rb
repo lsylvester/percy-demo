4e86162c9b4d4f24487eabed70c015c4bf31dbca
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2016-09-22 15:12:58 +1000

    Use a reporter to try to force sending results to Percy

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,26 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
-Percy::Capybara.initialize_build
-MiniTest.after_run { Percy::Capybara.finalize_build }
+
+class PercyReporter
+  def start
+    Percy::Capybara.initialize_build
+  end
+
+  def record(_)
+  end
+
+  def report
+    Percy::Capybara.finalize_build
+  end
+
+  def passed?
+    true
+  end
+end
+
+Minitest.reporter.reporters << PercyReporter.new
+
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
@@ -14,5 +32,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # def after_suite
+  #   binding.pry
+  # end
   # Add more helper methods to be used by all tests here...
 end
